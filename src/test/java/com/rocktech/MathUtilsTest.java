af2cdf0d8464a68992d248a99b1e372ce8ccf00f
hardeylarning <hardeylarning@gmail.com>
2022-07-01 10:43:51 +0100

    up

--- src/test/java/com/rocktech/MathUtilsTest.java
+++ src/test/java/com/rocktech/MathUtilsTest.java
@@ -6,15 +6,18 @@ import org.junit.jupiter.api.*;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
-/** TestInstance
- * TestInstance annotation with TestInstance.Lifecycle.PER_CLASS
- * permits singleton of creating only one instance for the class
+/** TestInstance.
+ * TestInstance annotation with TestInstance.Lifecycle.PER_CLASS,
+ * permits singleton by creating only one instance for the class,
+ * but uses TestInstance.Lifecycle.PER_METHOD by default
 */
 
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 public class MathUtilsTest {
 
     MathUtils mathUtils;
+    TestInfo testInfo;
+    TestReporter testReporter;
 
     // beforeAll must be static in order to run because it would run before instance is created
     @BeforeAll
@@ -24,8 +27,11 @@ public class MathUtilsTest {
 
     // execute this before any method runs
     @BeforeEach
-    void init(){
+    void init(TestInfo testInfo, TestReporter testReporter){
+        this.testInfo = testInfo;
+        this.testReporter = testReporter;
         mathUtils = new MathUtils();
+        testReporter.publishEntry("Running "+ testInfo.getDisplayName() + " with tags "+testInfo.getTags());
     }
 
     @AfterEach
@@ -34,6 +40,7 @@ public class MathUtilsTest {
     }
 
     @Test
+    @Tag("Math")
     public void testAdd(){
         int expected = 10;
         int actual = mathUtils.add(-1, 11);
@@ -41,11 +48,22 @@ public class MathUtilsTest {
     }
 
     @Test
+    @Tag("Math")
+    public void testMultiply(){
+        System.out.println("Running "+ testInfo.getDisplayName() + " with tags "+testInfo.getTags());
+        int expected = 20;
+        int actual = mathUtils.multiply(4, 5);
+        assertEquals(expected, actual, "The multiply method should return appropriate result");
+    }
+
+    @Test
+    @Tag("Math")
     public void divideTest(){
         assertThrows(ArithmeticException.class, () -> mathUtils.divide(1, 0), "Divide by zero should throw an error");
     }
 
     @Test
+    @Tag("Circle")
     public void testComputeCircleRadius(){
         assertEquals(314.1592653589793, mathUtils.computerCircleArea(10), "should return right circle area");
     }
